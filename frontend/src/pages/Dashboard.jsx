a429065d8c62f152b8d5c18252e7647257a63944
MBrillian354 <brillian354@gmail.com>
2025-07-31 14:58:08 +0700

    Adjust admin dashboard to accept a list of objects

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,17 @@
 import StatsCard from '../components/StatsCard';
 
 export default function Dashboard() {
-  const statsData = [
-    { label: "Supervisors", value: 85 },
-    { label: "Staffs", value: 31 },
-    { label: "Job Titles", value: 7 },
-    { label: "Supervisors", value: 85 },
-    { label: "Staffs", value: 31 }
-  ];
+  const statsData = {
+    row1: [
+      { label: "Supervisors", value: 85 },
+      { label: "Staffs", value: 31 }
+    ],
+    row2: [
+      { label: "Job Titles", value: 7 },
+      { label: "Supervisors", value: 85 },
+      { label: "Staffs", value: 31 }
+    ]
+  };
 
   return (
     <div className="container mx-auto">
@@ -15,15 +19,15 @@ export default function Dashboard() {
         Welcome back, <span className="underline">Admin</span>.
       </h1>
 
-      <div className="flex flex-col gap-4 mt-6Ou">
+      <div className="flex flex-col gap-4 mt-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {statsData.slice(0, 2).map((stat, index) => (
+          {statsData.row1.map((stat, index) => (
             <StatsCard key={index} label={stat.label} value={stat.value} />
           ))}
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {statsData.slice(2, 5).map((stat, index) => (
-            <StatsCard key={index + 2} label={stat.label} value={stat.value} />
+          {statsData.row2.map((stat, index) => (
+            <StatsCard key={index} label={stat.label} value={stat.value} />
           ))}
         </div>
       </div>

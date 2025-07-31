7795de2d734c92ed9dabb2b7e6a955539bc524c2
MBrillian354 <brillian354@gmail.com>
2025-07-31 14:56:21 +0700

    Add admin pages
    Add Dashbaord Page
    Add Jobs Page
    Add Accounts Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
 import StaffView from './pages/StaffView'
+import JobsView from './pages/JobsView'
+import AccountsView from './pages/AccountsView'
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <div className='min-h-screen bg-background text-gray-900 font-inter max-w-6xl mx-auto'>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/staff" element={<StaffView />} />
+          <Route path="/jobs" element={<JobsView />} />
+          <Route path="/accounts" element={<AccountsView />} />
         </Routes>
       </div>
     </Router>

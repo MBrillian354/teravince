031e80bac533a20b96cfd747c9ffaea1bc8ac386
MBrillian354 <brillian354@gmail.com>
2025-08-01 14:34:05 +0700

    Define routes for task schema

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, getAllTasks } = require('../controllers/taskController');
 
-// Endpoint untuk membuat task
+const { getAllTasks, getTaskById, getTasksByStaffId, getTaskByIdAndStaffId, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+
+
+router.get('/', getAllTasks);
+router.get('/:id', getTaskById);
+
+router.get('/:staffId/tasks', getTasksByStaffId);
+
+router.get('/:staffId/tasks/:taskId', getTaskByIdAndStaffId);
+
+
 router.post('/', createTask);
 
-// Endpoint untuk melihat semua task
-router.get('/', getAllTasks);
+router.put('/:id', updateTask);
+
+router.delete('/:id', deleteTask);
+
 
 module.exports = router;

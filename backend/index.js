bc415c43accbb9014bafdbb9b4903842e035f18a
MBrillian354 <brillian354@gmail.com>
2025-08-01 14:54:29 +0700

    Log db info whether it is local or atlas

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection string
-const MONGO_URI = 'mongodb://127.0.0.1:27017/teravince';
-// const MONGO_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.eupjv.mongodb.net:27017,cluster0-shard-00-01.eupjv.mongodb.net:27017,cluster0-shard-00-02.eupjv.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-cph2wz-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+// const MONGO_URI = 'mongodb://127.0.0.1:27017/teravince';
+const MONGO_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.eupjv.mongodb.net:27017,cluster0-shard-00-01.eupjv.mongodb.net:27017,cluster0-shard-00-02.eupjv.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-cph2wz-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+// Determine if the DB connection is local or Atlas
+const isLocalDB = MONGO_URI.includes('127.0.0.1') || MONGO_URI.includes('localhost');
 
 // Middleware
 app.use(cors());
@@ -46,7 +48,7 @@ app.use((err, req, res, next) => {
 // Connect and start server
 mongoose.connect(MONGO_URI)
     .then(() => {
-        console.log(`Connected to MongoDB`);
+        console.log(`Connected to MongoDB (${isLocalDB ? 'Local' : 'Atlas'})`);
         app.listen(PORT, () => {
             console.log(`Server is running at http://localhost:${PORT}`);
         });

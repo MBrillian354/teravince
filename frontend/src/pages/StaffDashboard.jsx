ce139350b713c366150f871a5ae8a2b046a204c8
MBrillian354 <brillian354@gmail.com>
2025-08-07 16:41:13 +0700

    Display job title in staff dashboard

--- frontend/src/pages/StaffDashboard.jsx
+++ frontend/src/pages/StaffDashboard.jsx
@@ -16,10 +16,11 @@ const StaffDashboard = () => {
 
   return (
     <div className={containerStyle}>
-      <div className="flex justify-between items-center my-6">
+      <div className="flex flex-col justify-between items-start my-6">
         <h1 className="page-title">
           Welcome back, <span className="underline">{staff.firstName + ' ' + staff.lastName}</span>.
         </h1>
+        <h2 className='text-black text-xl font-montserrat font-semibold'>{staff.jobId.title}</h2>
       </div>
 
       {/* Main Dashboard Section */}
